b85d8dde1222865448d271f35ddc7107c3685544
Jorge15042001 <adrivulgarin@hotmail.com>
2021-10-31 02:02:24 -0500

    apt disgn

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,38 @@
+// const {Complex} = require("complex.js");
+
 const id_canvas_souce = "source_canvas";
 const id_canvas_operator = "operator_canvas";
 const id_canvas_result = "result_canvas";
 
+
+const s = ( sketch ) => {
+  let plot2d ; // handles all the mathematical aspect of the the plot
+
+  sketch.setup = () => {
+    sketch.createCanvas(200, 200);
+  };
+
+  sketch.draw = () => {
+    sketch.clear()
+  };
+
+  sketch.drawAxes(p0x,p1x,p0y,p1y){
+    //draw axis lines
+    //draw arrows
+  }
+  sketch.drawLine(l){
+    for (let i =0; i<l.length-1;i++)
+    {
+      const p0 = l[i];
+      const p1 = l[i+1];
+      sketch.line(p0.re,p0.im,p1.re,p1.im);
+    }
+  }
+
+
+
+
+};
+
+let myp5 = new p5(s,id_canvas_souce);
 
--- scripts/plot.js
+++ scripts/plot.js
@@ -1,10 +1,33 @@
 class plot2d {
-  constructor(canvas) {
-    this.p5Canvas = canvas;
-    //TODO: set to the center of the canvas
+  constructor(sketch,size,origin) {
     this.origin = createVecor(0, 0);
-
+    this.lines = [];
+    this.sketch = sketch;
+    this.scaleFactor = 1;
   }
+  addPoint(p){
+    const lastLine = this.lines[this.lines.length -1];
+    lastLine[lastLine.length ] = p;
+  }
+  beginNewLine(){
+    this.lines.push([]);
+  }
+
+  zoomIn(){
+    this.scaleFactor += 0.1;
+  }
+  zoomOut(){
+    this.scaleFactor -= 0.1;
+  }
+
+  drawLines(){
+    for (l of this.lines){
+      this.sketch.drawLine(l);
+    }
+  }
+
+
+  
 
 
 }
